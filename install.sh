171943fe6c2f137ca2a9da133bcfbcde7b47ada1
Avaera <johan.sorman@icloud.com>
2025-08-23 14:06:03 +0000

    ...

--- install.sh
+++ install.sh
@@ -15,10 +15,6 @@ echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
 git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
-echo -e "\nCloning Titus-Omarchy from: https://github.com/${TITUS_REPO}.git"
-rm -rf ~/.local/share/omarchy-titus/
-git clone "https://github.com/${TITUS_REPO}.git" ~/.local/share/omarchy-titus >/dev/null
-
 # Use custom branch if instructed
 if [[ -n "$OMARCHY_REF" ]]; then
   echo -e "\nUsing branch: $OMARCHY_REF"
@@ -40,52 +36,58 @@ source $OMARCHY_INSTALL/preflight/presentation.sh
 source $OMARCHY_INSTALL/preflight/migrations.sh
 
 # Configuration
-#show_logo beams 240
-#show_subtext "Let's install! [1/5]"
 source $OMARCHY_INSTALL/config/identification.sh
 source $OMARCHY_INSTALL/config/config.sh
 source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
-#source $OMARCHY_INSTALL/config/fix-fkeys.sh # only for apple 
 source $OMARCHY_INSTALL/config/network.sh
 source $OMARCHY_INSTALL/config/power.sh
 source $OMARCHY_INSTALL/config/timezones.sh
-#source $OMARCHY_INSTALL/config/login.sh
 source $OMARCHY_INSTALL/config/nvidia.sh
 
 # Development
-#show_logo decrypt 920
-#show_subtext "Installing terminal tools [2/5]"
 source $OMARCHY_INSTALL/development/terminal.sh
 source $OMARCHY_INSTALL/development/development.sh
-#source $OMARCHY_INSTALL/development/nvim.sh
 source $OMARCHY_INSTALL/development/ruby.sh
 source $OMARCHY_INSTALL/development/docker.sh
 source $OMARCHY_INSTALL/development/firewall.sh
 
 # Desktop
-#show_logo slice 60
-#show_subtext "Installing desktop tools [3/5]"
 source $OMARCHY_INSTALL/desktop/desktop.sh
 source $OMARCHY_INSTALL/desktop/hyprlandia.sh
 source $OMARCHY_INSTALL/desktop/theme.sh
 source $OMARCHY_INSTALL/desktop/bluetooth.sh
-source $OMARCHY_INSTALL/desktop/asdcontrol.sh
 source $OMARCHY_INSTALL/desktop/fonts.sh
 source $OMARCHY_INSTALL/desktop/printer.sh
 
+# Removes Chromium and install Firefox
+if pacman -Qi chromium &>/dev/null; then
+    sudo pacman -Rns --noconfirm chromium
+fi
+yay -S --noconfirm --needed firefox
+
+# Install hyprpolkitagent
+if pacman -Qi hyprpolkitagent &>/dev/null; then
+    exit 1
+else
+    yay -S hyprpolkitagent
+fi
+
 # Apps
-#show_logo expand
-#show_subtext "Installing default applications [4/5]"
-#source $OMARCHY_INSTALL/apps/webapps.sh
-#source $OMARCHY_INSTALL/apps/xtras.sh
 source $OMARCHY_INSTALL/apps/mimetypes.sh
 
+# Installing Rofi and my config
+yay -S --noconfirm --needed rofi-wayland
+mkdir -p ~/.config/rofi
+wget -O ~/.config/rofi/config.rasi https://raw.githubusercontent.com/avaera8775/dwm-setup/refs/heads/main/suckless/rofi/config.rasi
+wget -O ~/.config/rofi/power.rasi https://raw.githubusercontent.com/avaera8775/dwm-setup/refs/heads/main/suckless/rofi/power.rasi
+
 # Updates
-#show_logo highlight
-#show_subtext "Updating system packages [5/5]"
 sudo mkinitcpio -P
 sudo updatedb
 sudo pacman -Syu --noconfirm
+
+# Link overwrite config
+ln -s $(pwd)/hypr $HOME/.config/
 
 # Reboot
 echo "Installation complete! Reboot when ready."
--- setup-link.sh
+++ setup-link.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 ln -s $(pwd)/hypr $HOME/.config/
-git clone https://github.com/christitustech/nord-background.git ./tmp
-cp -r ./tmp/*.png $HOME/.local/share/omarchy/themes/nord/backgrounds
-rm -rf ./tmp
